02f09dcd382ff1a8ddc57464ccbe5230304accf1
Kaiky <kaikybatistacosta123@gmail.com>
2025-05-20 17:49:32 -0300

    Add files via upload

---
+++ assets/js/code.js
@@ -0,0 +1,13 @@
+function removerAOSSeMenorQue992() {
+  if (window.innerWidth <= 992) {
+    document.querySelectorAll('.css,.foto,.text-biografia,.Titulo' ).forEach(element => {
+      element.removeAttribute('data-aos');
+    });
+  }
+}
+
+// Executa ao carregar a página
+window.addEventListener('load', removerAOSSeMenorQue992);
+
+// Executa ao redimensionar a janela (opcional)
+window.addEventListener('resize', removerAOSSeMenorQue992);
